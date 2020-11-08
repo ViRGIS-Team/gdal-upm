fc9e47f4180dd337930cab1f116374565cde3543
Paul <runette@gmail.com>
2020-11-08 14:35:55 +0000

    Update install_script.ps1

--- Assets/Scripts/install_script.ps1
+++ Assets/Scripts/install_script.ps1
@@ -1,37 +1,36 @@
 Param( 
     [string]$package,
-    [string]$install, 
+    [string]$install,  
     [string]$destination,
     [string]$test,
     [string]$shared_assets
-    ) 
+    )
 
 conda create --name upm -y
-conda activate upm
-conda install $install -y
+conda install --name upm $install -y
 
-
-$env = conda info --json | ConvertFrom-Json 
-$conda_bin = -join($env.active_prefix, "\Library\bin")
+$env = conda info --envs
+$temp = conda info --envs | Select-String  -Pattern 'upm'
+$temp -match "C\:.*"
+$conda_bin = -join($matches[0], "\Library/bin")
 
 Write-Output "Processing *.dll"
-$file = -join($conda_bin, '/*.dll' )
+$file = -join($conda_bin, '\*.dll' )
 Write-Output "Copy $file to $destination"
 Copy-Item $file -Destination $destination -Force
 
 Write-Output "Processing $test"
-$file = -join($conda_bin, '/', $test )
+$file = -join($conda_bin, '\', $test )
 Copy-Item $file -Destination $destination -Force
  
 Write-Output "Processing gdal data"
-$file = -join($env.active_prefix, "\Library\share\gdal")
+$file = -join($matches[0], "\Library\share\gdal")
 Write-Output "Copy $file to $shared_assets"
 Copy-Item -Path $file -Destination $shared_assets -Recurse -Force
 
 Write-Output "Processing proj data"
-$file = -join($env.active_prefix, "\Library\share\proj")
+$file = -join($matches[0], "\Library\share\proj")
 Write-Output "Copy $file to $shared_assets"
 Copy-Item -Path $file -Destination $shared_assets -Recurse -Force
 
-conda deactivate
 conda remove --name upm --all -y
