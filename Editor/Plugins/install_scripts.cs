0ebd4bfebad6fb4009e0dcbeed928d9ac6ae25fe
runette <runette@gmail.com>
2021-05-12 00:42:24 +0100

    Version 3 of the scripts

--- Editor/Plugins/install_scripts.cs
+++ Editor/Plugins/install_scripts.cs
@@ -4,24 +4,15 @@ using System.IO;
 using System.Diagnostics;
 using System;
 using Debug = UnityEngine.Debug;
+using OSGeo.GDAL;
 
 #if UNITY_EDITOR
-namespace Test {
+namespace OSGeo.Install {
 
     public class Install{
 
-#if UNITY_EDITOR_WIN
-        const string test = "gdalinfo.exe";
-#elif UNITY_EDITOR_OSX
-        const string test = "gdalinfo";
-        const string basharg = "-l";
-#elif UNITY_EDITOR_LINUX
-        const string test = "gdalinfo";
-        const string basharg = "-i";
-#endif
-
         const string packageVersion = "1.0.0";
-        const string versionString = "3.2.2";
+        const string versionString = "3020200";
 
         [InitializeOnLoadMethod]
         static void OnProjectLoadedinEditor()
@@ -33,58 +24,19 @@ namespace Test {
             if (Application.isEditor) {
                 try
                 {
-                    string pluginPath = Path.Combine(Application.dataPath, "Conda");
-                    if (!Directory.Exists(pluginPath)) Directory.CreateDirectory(pluginPath);
-#if UNITY_EDITOR_WIN
-                    string file = Path.Combine(pluginPath, "Library", "bin", test);
-                    string file2 = Path.Combine(pluginPath, "Library", "bin", "gdal_test.exe");
-#else
-                    string file = Path.Combine(pluginPath, "bin", test);
-                    string file2 = Path.Combine(pluginPath, "lib", "gdal_test.exe");
-#endif
-                    if (!File.Exists(file2))
+                    string currentVersion = Gdal.VersionInfo(null);
+                    if (currentVersion != versionString)
                     {
                         UpdatePackage();
                         AssetDatabase.Refresh();
                     }
-                    else if (!EditorApplication.isPlayingOrWillChangePlaymode)
-                    {
-                        string currentVersion = "0";
-                        string response;
-                        try
-                        {
-                            using (Process compiler = new Process())
-                            {
-                                compiler.StartInfo.FileName = file;
-                                compiler.StartInfo.Arguments = "--version";
-                                compiler.StartInfo.UseShellExecute = false;
-                                compiler.StartInfo.RedirectStandardOutput = true;
-                                compiler.StartInfo.CreateNoWindow = true;
-                                compiler.Start();
-
-                                response = compiler.StandardOutput.ReadToEnd();
-
-                                compiler.WaitForExit();
-                            }
-                            currentVersion = response.Split(new char[] { ' ', ',' })[1];
-                            Debug.Log($"GDAL Version : {currentVersion}");
-                        } catch (Exception e)
-                        {
-                            Debug.Log($"GDAL Version error {e.ToString()}");
-                        }
-                        if (currentVersion != versionString)
-                        {
-#if ! UNITY_EDITOR_WIN
-                            UpdatePackage();
-                            AssetDatabase.Refresh();
-#endif
-                        }
-                    }
+                    
                 }
                 catch (Exception e)
                 {
-                    // do nothing
-                    Debug.Log($"Error in Conda Package {test} : {e.ToString()}");
+                    Debug.Log($"Error in Conda Package GDAL: {e.ToString()}");
+                    UpdatePackage();
+                    AssetDatabase.Refresh();
                 };
             };
 
@@ -92,38 +44,18 @@ namespace Test {
             stopwatch.Stop();
             Debug.Log($"Gdal refresh took {stopwatch.Elapsed.TotalSeconds} seconds");
         }
-        static void UpdatePackage() {
-            string pluginPath = Path.Combine(Application.dataPath, "Conda");
+
+
+        static void UpdatePackage()
+        {
+            Debug.Log("Mdal Install Script Awake");
             string path = Path.GetDirectoryName(new StackTrace(true).GetFrame(0).GetFileName());
-            string exec = Path.Combine(path, "install_script.ps1");
-            string response;
-            string install = $"gdal-csharp={packageVersion}";
-            using (Process compiler = new Process())
-            {
 #if UNITY_EDITOR_WIN
-                compiler.StartInfo.FileName = "powershell.exe";
-                compiler.StartInfo.Arguments = $"-ExecutionPolicy Bypass \"{Path.Combine(path, "install_script.ps1")}\" -package gdal " +
-                                                    $"-install {install} " +
-                                                    $"-destination '{pluginPath}' " +
-                                                    $"-shared_assets '{Application.streamingAssetsPath}' ";
+            path = Path.Combine(path, "install_script.ps1");
 #else
-                compiler.StartInfo.FileName = "/bin/bash";
-                compiler.StartInfo.Arguments = $" {basharg} '{Path.Combine(path, "install_script.sh")}' " +
-                                                $"-p gdal " +
-                                                $"-i {install} " +
-                                                $"-d '{pluginPath}' " +
-                                                $"-s '{Application.streamingAssetsPath}'  ";
-
+            path = Path.Combine(path, "install_script.sh");
 #endif
-                compiler.StartInfo.UseShellExecute = false;
-                compiler.StartInfo.RedirectStandardOutput = true;
-                compiler.StartInfo.CreateNoWindow = true;
-                compiler.Start();
-
-                response = compiler.StandardOutput.ReadToEnd();
-
-                compiler.WaitForExit();
-            }
+            string response = Conda.Conda.Install($"gdal-csharp={packageVersion}", path);
         }
     }
 }
